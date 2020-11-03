f1071fc2e7c87f4c0b60eaf715ab23225f33a863
xiaoshuai.chen <xiaoshuai.chen@vhall.com>
2020-11-03 13:42:47 +0800

    test

---
+++ src/components/MenuItem/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <el-submenu class="li" :index="item.index" v-if="item.children">
+    <template slot="title">
+      <i :class="item.iconClass"></i>
+      <span>{{ item.title }}</span>
+    </template>
+    <template v-if="item.children">
+      <default-home v-for="(item,index) in item.children" :item="item" :key="index"></default-home>
+    </template>
+  </el-submenu>
+  <el-menu-item v-else :index="item.index">
+    <router-link :to="{ path: item.path }" class="cxs-a-line">
+      <i :class="item.iconClass"></i>
+      <span>{{ item.title }}</span>
+    </router-link>
+  </el-menu-item>
+</template>
+<script>
+export default {
+  props: ["item"],
+  name: "defaultHome",
+  data() {
+    return {};
+  },
+  mounted() {
+    // console.log(this.$route, 3333);
+  },
+  methods: {
+    setAddress(param) {
+      console.log(param, "最终点击的设置值");
+      // localStorage.setItem("localAddress", JSON.stringify(to.matched));
+    }
+  }
+};
+</script>
+<style lang="less" scoped>
+/deep/.el-submenu,
+.el-submenu .el-menu-item {
+  padding: 0;
+}
+.cxs-a-line {
+  display: inline-block;
+  width: 100%;
+  height: 100%;
+}
+.router-link-active {
+  color: orangered;
+}
+</style>
