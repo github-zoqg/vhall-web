e0cedf03cfa5172b967f65996fa6c06bd0a08911
xiao.zhang <xiao.zhang@vhall.com>
2020-11-04 10:09:09 +0800

    '修改'

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,7 @@
 <template>
-  <el-row :gutter="20">
-    <el-col :span="18"><div class="grid-content bg-purple">1111</div></el-col>
-    <el-col :span="6"><div class="grid-content bg-purple">2222</div></el-col>
-  </el-row>
-  <!-- <div class="about">
+  <div class="about">
     <h1>This is an about page</h1>
-  </div> -->
+  </div>
 </template>
 <style lang="less" scoped>
 h1{
