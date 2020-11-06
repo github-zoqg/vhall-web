1ada5dcd89ddf53f5936e15b7b12c90e767beb5d
jia.li <jia.li@vhall.com>
2020-11-06 13:26:54 +0800

    '左侧导航调整，目录名称修改'

--- src/views/LiveModule/customTab.vue
+++ src/views/LiveModule/customTab.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
-    自定义菜单
+    <div class="title--cus-tab">
+      <page-title title="自定义菜单">
+        <div slot="content">所有设置对电脑端和移动浏览器同时生效</div>
+      </page-title>
+      <el-button type="primary" plain>主要按钮</el-button>
+    </div>
   </div>
 </template>
 
 <script>
+import PageTitle from "@/views/LiveModule/components/pageTitle";
 export default {
-  name: "customTab.vue"
+  name: "customTab.vue",
+  components: {PageTitle}
 };
 </script>
 
