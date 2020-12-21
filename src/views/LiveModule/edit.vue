2611d20cf01b0b418fc641b00bd8a48a186add56
xiaodong.ding <xiaodong.ding@vhall.com>
2020-12-21 22:17:44 +0800

    直播回放发布功能

--- src/views/LiveModule/edit.vue
+++ src/views/LiveModule/edit.vue
@@ -338,6 +338,13 @@ export default {
       this.title = '创建';
       this.webinarId = '';
     }
+    if (this.$route.query.record_id) {
+      this.selectMedia = {
+        id: this.$route.query.record_id,
+        paas_record_id: this.$route.query.paas_record_id,
+        name: this.$route.query.name
+      }
+    }
     console.log(JSON.parse(sessionOrLocal.get('SAAS_VS_PES', 'localStorage')).new_interact, '????????????')
   },
   methods: {
