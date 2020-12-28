2ae7b5a49f36a36c6fcca59e4789e092699d3d90
zhiqiang.hao <zhiqiang.hao@vhall.com>
2020-12-29 00:52:53 +0800

    修改自定义菜单样式

---
+++ src/views/LiveModule/Room/components/custom-menu/component-desimg.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="component-wrap-box">
+    <!-- 显示区域 -->
+    <div class="editor-preview" v-html="info.content || '默认图文'">
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    info: {
+      required: false
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+  .component-wrap-box{
+    width: 100%;
+    color: #333;
+    text-align: left;
+  }
+  .editor-preview{
+    min-height: 30px;
+    margin: 20px 0px;
+    font-size: 14px;
+    font-family: PingFangSC-Regular, PingFang SC;
+    font-weight: 400;
+    color: #1A1A1A;
+    line-height: 22px;
+  }
+</style>
