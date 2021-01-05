7374e048c0b5666f36aa0dc72aa9249a36d42c51
jia.li <jia.li@vhall.com>
2021-01-05 17:05:33 +0800

    品牌设置

--- src/views/LiveModule/components/brandSetPreview.vue
+++ src/views/LiveModule/components/brandSetPreview.vue
@@ -98,8 +98,8 @@
       <div class="app" v-show="switchType === 'app'">
         <div :class="`skin-preview preview-${switchType} ${bgColorType}`">
           <div class="panel__preview">
-            <header class="h5-title"  v-if="signSetVo && signSetVo.view_status == 1">
-              <img :src="signSetVo.logo_url" alt v-if="signSetVo && signSetVo.logo_url" class="logo"/>
+            <header class="h5-title"  v-if="signSetVo && signSetVo.organizers_status == 1">
+              <img :src="domain_url || signSetVo.logo_url" alt v-if="(signSetVo && signSetVo.logo_url) || domain_url" class="logo"/>
               <span class="title">北京微吼时代科技有限公司</span>
               <div class="title-icons" :style="{'color': skinSetVo.pageStyle }">
                 <icon icon-class="saasguanzhu"></icon>
