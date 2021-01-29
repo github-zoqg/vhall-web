13e1c3421f44d5e3d7567a8b17b04852c8ee741b
xiao.zhang <xiao.zhang@vhall.com>
2021-01-29 16:05:22 +0800

    修改图片地址

--- src/views/LiveModule/CustomerTabView/components/componet-editor/component-imglink.vue
+++ src/views/LiveModule/CustomerTabView/components/componet-editor/component-imglink.vue
@@ -127,6 +127,10 @@ export default {
 </script>
 <style lang="less" scoped>
   .qr-previewbox{
+    img{
+      width: 100%;
+      height: 100%;
+    }
   }
   /deep/.el-upload--picture-card i.saasicon_shangchuan {
     font-size: 36px;
