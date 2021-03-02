b590fe880f5776aacd858a9fe54aa837af9d8097
xiaoshuai.chen <xiaoshuai.chen@vhall.com>
2021-03-02 16:02:56 +0800

    修改参数名字

--- src/views/LiveModule/Room/embedClient.vue
+++ src/views/LiveModule/Room/embedClient.vue
@@ -534,7 +534,7 @@ export default {
             this.photoCpathaShow = true
             sessionOrLocal.set('sso', res.data.sso_token)
             sessionOrLocal.set('token', res.data.token, 'localStorage')
-            sessionOrLocal.set('tokenExpiredTime', res.data.exp, 'localStorage')
+            sessionOrLocal.set('tokenExpiredTime', res.data.exp_time, 'localStorage')
             // sessionOrLocal.set('userInfo', res.data)
             this.fetchData()
 
