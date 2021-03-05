6666b54c76f38581353e3ee46900465f96d6f10c
xiaodong.ding <xiaodong.ding@vhall.com>
2021-03-05 14:50:54 +0800

    客户端下载地址获取接口联调

--- src/api/config.js
+++ src/api/config.js
@@ -93,6 +93,7 @@ const apis = {
   liveDel: ['/v3/webinars/webinar/delete', 'POST'], // 活动删除  √
   demandCreate: ['/v3/webinars/webinar/create-demand', 'POST'], // 点播创建
   checkLive: ['/v3/webinars/live/check', 'POST'], // 进入直播前检测
+  getPCDownloadUrl: ['/v3/commons/version/upgrade', 'POST'], // 客户端下载地址获取接口
 
   // 专题
   subjectCreate: ['/v3/webinars/subject/create', 'POST'], // 专题创建
