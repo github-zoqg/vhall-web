ad93e9cc068c89d4a22bbac8de7fa25538217423
xiao.zhang <xiao.zhang@vhall.com>
2021-03-08 17:56:15 +0800

    修改favicon

--- vue.config.js
+++ vue.config.js
@@ -63,6 +63,15 @@ module.exports = {
       }
     }
   },
+  pwa: {
+    iconPaths: {
+      favicon32: 'favicon.ico',
+      favicon16: 'favicon.ico',
+      appleTouchIcon: 'favicon.ico',
+      maskIcon: 'favicon.ico',
+      msTileImage: 'favicon.ico'
+    }
+  },
   chainWebpack: config=>{
     config.plugin('html').tap(options=>{
       options[0].cdn = cdn
