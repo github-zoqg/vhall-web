aa75066b83e7553fff83952a1cd79937d37ff905
xiao.zhang <xiao.zhang@vhall.com>
2021-03-15 17:11:25 +0800

    修改播放器和用户统计选择框

--- src/views/LiveModule/Brands/playerSet.vue
+++ src/views/LiveModule/Brands/playerSet.vue
@@ -360,7 +360,7 @@ export default {
         imageUrl: '',
       },
       videoParam: {
-        paas_record_id: process.env.NODE_ENV === 'development' ? '27d23478' : '4b39a1a'
+        paas_record_id: process.env.VUE_APP_NODE_ENV === 'production' ? '4b39a1a' : '27d23478'
       },
       marqueeOption: {
         enable: Boolean(this.scrolling_open),
