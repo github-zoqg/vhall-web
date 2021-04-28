c65a7774658f70769de4ee61b9e32f6e3e103134
xiao.zhang <xiao.zhang@vhall.com>
2021-04-28 10:40:24 +0800

    修改

--- src/router/modules/material.js
+++ src/router/modules/material.js
@@ -10,43 +10,43 @@ const router = [
       {
         path: 'word',
         component: () => import('@/views/MaterialModule/word'),
-        meta: { auth: true, title: '文档', name: 'wordMgr' }
+        meta: { auth: true, title: '文档', name: 'wordMgr', k: 100510}
       },
       {
         path: 'video',
         component: () => import('@/views/MaterialModule/video'),
-        meta: { auth: true, title: '音视频', name: 'videoMgr'}
+        meta: { auth: true, title: '音视频', name: 'videoMgr', k: 100517}
       },
       {
         path: 'question',
         component: () => import('@/views/MaterialModule/question'),
-        meta: { auth: true, title: '问卷', name: 'questionMgr', auth_key: 'ui.hide_survey'}
+        meta: { auth: true, title: '问卷', name: 'questionMgr', auth_key: 'ui.hide_survey', k: 100525}
       },
       {
         path: 'addQuestion',
         component: () => import('@/views/MaterialModule/addQuestion'),
-        meta:{ auth: true, title: '创建问卷', name: 'addQuestionMgr', auth_key: 'ui.hide_survey' },
+        meta:{ auth: true, title: '创建问卷', name: 'addQuestionMgr', auth_key: 'ui.hide_survey'},
         hidden: true
       },
       {
         path: 'prize',
         component: () => import('@/views/MaterialModule/prize'),
-        meta: { auth: true, title: '奖品', name: 'prizeMgr', auth_key: 'ui.hide_lottery'}
+        meta: { auth: true, title: '奖品', name: 'prizeMgr', auth_key: 'ui.hide_lottery', k: 100533}
       },
       {
         path: 'viewer',
         component: () => import('@/views/MaterialModule/viewer'),
-        meta: { auth: true, title: '观众', name: 'viewerMgr', auth_key: 'white_list'}
+        meta: { auth: true, title: '观众', name: 'viewerMgr', auth_key: 'white_list', k: 100540}
       },
       {
         path: 'advertCard',
         component: () => import('@/views/LiveModule/Brands/advertisementCard'),
-        meta: { auth: true, title: '广告', name: 'advertCard', auth_key: 'ad_recommend'}
+        meta: { auth: true, title: '广告', name: 'advertCard', auth_key: 'ad_recommend', k: 100550}
       },
       {
         path: 'gift',
         component: () => import('@/views/MaterialModule/gift'),
-        meta: { auth: true, title: '礼物', name: 'giftMgr', auth_key: 'ui.show_gift' }
+        meta: { auth: true, title: '礼物', name: 'giftMgr', auth_key: 'ui.show_gift', k: 100556}
       }
     ]
   }
