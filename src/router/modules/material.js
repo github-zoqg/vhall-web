930c37877f12f7f0bc31de581f91bfca1fc7ea36
xiao.zhang <xiao.zhang@vhall.com>
2021-05-10 20:22:54 +0800

    修改资料埋点

--- src/router/modules/material.js
+++ src/router/modules/material.js
@@ -10,17 +10,17 @@ const router = [
       {
         path: 'word',
         component: () => import('@/views/MaterialModule/word'),
-        meta: { auth: true, title: '文档', name: 'wordMgr', k: 100510}
+        meta: { auth: true, title: '文档', name: 'wordMgr'}
       },
       {
         path: 'video',
         component: () => import('@/views/MaterialModule/video'),
-        meta: { auth: true, title: '音视频', name: 'videoMgr', k: 100517}
+        meta: { auth: true, title: '音视频', name: 'videoMgr'}
       },
       {
         path: 'question',
         component: () => import('@/views/MaterialModule/question'),
-        meta: { auth: true, title: '问卷', name: 'questionMgr', auth_key: 'ui.hide_survey', k: 100525}
+        meta: { auth: true, title: '问卷', name: 'questionMgr', auth_key: 'ui.hide_survey'}
       },
       {
         path: 'addQuestion',
@@ -31,22 +31,22 @@ const router = [
       {
         path: 'prize',
         component: () => import('@/views/MaterialModule/prize'),
-        meta: { auth: true, title: '奖品', name: 'prizeMgr', auth_key: 'ui.hide_lottery', k: 100533}
+        meta: { auth: true, title: '奖品', name: 'prizeMgr', auth_key: 'ui.hide_lottery'}
       },
       {
         path: 'viewer',
         component: () => import('@/views/MaterialModule/viewer'),
-        meta: { auth: true, title: '观众', name: 'viewerMgr', auth_key: 'white_list', k: 100540}
+        meta: { auth: true, title: '观众', name: 'viewerMgr', auth_key: 'white_list'}
       },
       {
         path: 'advertCard',
         component: () => import('@/views/LiveModule/Brands/advertisementCard'),
-        meta: { auth: true, title: '广告', name: 'advertCard', auth_key: 'ad_recommend', k: 100550}
+        meta: { auth: true, title: '广告', name: 'advertCard', auth_key: 'ad_recommend'}
       },
       {
         path: 'gift',
         component: () => import('@/views/MaterialModule/gift'),
-        meta: { auth: true, title: '礼物', name: 'giftMgr', auth_key: 'ui.show_gift', k: 100556}
+        meta: { auth: true, title: '礼物', name: 'giftMgr', auth_key: 'ui.show_gift'}
       }
     ]
   }
