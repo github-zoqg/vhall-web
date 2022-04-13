1e28f29696e786246c4ec8a0e7e9e954a096e0f0
liwei.ding <liwei.ding@vhall.com>
2022-04-13 16:34:16 +0800

    批量分配

--- src/components/GroupingCard/index.vue
+++ src/components/GroupingCard/index.vue
@@ -1,70 +1,75 @@
 <template>
   <div class="grouping-card">
     <!--小组卡片-->
-    <draggable class="list-group"
-      :list="list"
-      :move="groupMove"
-      @start="groupStart"
-      @end="groupEnd"
-      :sort="!sort"
-      draggable=".item"
-      :groupName="groupName"
-      ghostClass="ghost"
-      group="a">
-      <!--分配小组按钮组-->
-      <div slot="header"
-        class="btn-group"
-        role="group">
-        <span>{{groupName}}（{{list.length}}）</span>
-        <div class="btn-group-right">
-          <span v-if="!batchGroupState"
-            @click="batchGroup"><i class="vh-saas-iconfont vh-saas-a-line-batchdistribution pr4"></i>{{groupType?'批量换组':'批量分配'}}</span>
-          <span v-if="groupType&&!batchGroupState"
-            @click="dissolution"><i class="vh-saas-iconfont vh-saas-a-line-dissolutiongrouping pr4"></i>解散</span>
-          <span v-show="batchGroupState"
-            @click="batchGroupState = false"><i class="el-icon el-icon-close cancel-size"></i>取消</span>
-          <span v-show="batchGroupState"
-            @click="changeGroup"><i class="vh-saas-iconfont vh-saas-a-line-Ingroup pr4"></i>换组</span>
+    <el-checkbox-group class="list-group-item item"
+      v-model="checkList"
+      @change="changeCheck">
+      <draggable class="list-group"
+        :list="list"
+        :move="groupMove"
+        @start="groupStart"
+        @end="groupEnd"
+        :sort="!sort"
+        draggable=".item"
+        :groupName="groupName"
+        ghostClass="ghost"
+        group="a">
+        <!--分配小组按钮组-->
+        <div slot="header"
+          class="btn-group"
+          role="group">
+          <span class="group-header-name">{{groupName}}（{{list.length}}）</span>
+          <div class="btn-group-right">
+            <span v-if="!batchGroupState"
+              @click="batchGroup"><i class="vh-saas-iconfont vh-saas-a-line-batchdistribution pr4"></i>{{groupType?'批量换组':'批量分配'}}</span>
+            <span v-if="groupType&&!batchGroupState"
+              @click="dissolution"><i class="vh-saas-iconfont vh-saas-a-line-dissolutiongrouping pr4"></i>解散</span>
+            <span v-show="batchGroupState"
+              @click="batchGroupState = false"><i class="el-icon el-icon-close cancel-size"></i>取消</span>
+            <span v-show="batchGroupState"
+              @click="changeGroup"
+              :class="checkList&&checkList.length?'':'group-disable'"><i class="vh-saas-iconfont vh-saas-a-line-Ingroup pr4"></i>换组</span>
+          </div>
         </div>
-      </div>
-      <!--分配小组观众-->
-      <div class="list-group-item item"
-        v-for="item in list"
-        :key="item.name">
-        <el-checkbox v-if="batchGroupState"
-          :label="item.name"
-          size="medium"
-          :name="groupName">{{item.name}}</el-checkbox>
-        <div class="list-group-item-state"
-          v-if="!batchGroupState">
-          <el-popover placement="bottom-start"
-            width="100%"
-            :append-to-body="false"
-            :popper-options="{
+        <!--分配小组观众-->
+        <div class="list-group-item item"
+          v-for="item in list"
+          :key="item.name">
+          <el-checkbox v-if="batchGroupState"
+            :label="item.name"
+            size="medium"
+            :name="groupName">{{item.name}}</el-checkbox>
+          <div class="list-group-item-state"
+            v-if="!batchGroupState">
+            <el-popover placement="bottom-start"
+              width="100%"
+              :append-to-body="false"
+              :popper-options="{
               boundariesElement: 'body',
               gpuAcceleration:true,
               positionFixed:false,
               preventOverflow:true
             }"
-            popperClass="list-group-popover"
-            trigger="click">
-            <div class="list-group-item-button">
-              <div>移出小组</div>
-              <div>换组</div>
-            </div>
-            <span slot="reference">
-              <span class="list-group-name">{{item.name}}</span>
-              <!--<el-tooltip class="item"
+              popperClass="list-group-popover"
+              trigger="click">
+              <div class="list-group-item-button">
+                <div>移出小组</div>
+                <div>换组</div>
+              </div>
+              <span slot="reference">
+                <span class="list-group-name">{{item.name}}</span>
+                <!--<el-tooltip class="item"
                 effect="dark"
                 :content="item.name"
                 placement="top">
                 <span class="list-group-name">{{item.name}}</span>
               </el-tooltip>-->
-            </span>
-          </el-popover>
+              </span>
+            </el-popover>
+          </div>
         </div>
-      </div>
-    </draggable>
+      </draggable>
+    </el-checkbox-group>
     <!--换组-->
     <group-change ref="groupChange"></group-change>
   </div>
@@ -114,6 +119,7 @@ export default {
   },
   data() {
     return {
+      checkList: [],//复选数据
       batchGroupState: false,//批量换组,
       validMove: false,//拖动校验
       isStart: false,
@@ -121,11 +127,16 @@ export default {
     }
   },
   methods: {
+    /**复选事件 */
+    changeCheck(data) {
+      debugger
+    },
     batchGroup() {
       this.batchGroupState = true
     },
     /*换组*/
     changeGroup() {
+      if (!this.checkList.length) return
       this.$refs.groupChange.handleOpen()
       this.$emit('changeGroup')
     },
@@ -135,7 +146,6 @@ export default {
       console.log('解散' + this.groupName)
     },
     groupChange(data) {
-      debugger
       this.$emit('change', data)
     },
     groupStart() {
@@ -184,8 +194,20 @@ export default {
     line-height: 40px;
     border-bottom: 1px solid #ccc;
     overflow: hidden;
+    font-size: 14px;
     .btn-group-right {
       float: right;
+      height: 40px;
+      overflow: hidden;
+      span.group-disable {
+        color: gray;
+        padding-left: 8px;
+        font-size: 14px;
+        cursor: not-allowed;
+        &:hover {
+          color: gray;
+        }
+      }
       span {
         padding-left: 8px;
         font-size: 14px;
