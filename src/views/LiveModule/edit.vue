903563198bb88df13e7b1647dc5751773d584528
xushuo <893804934@qq.com>
2022-05-06 20:52:12 +0800

    测试用例涉及的显示逻辑修改

--- src/views/LiveModule/edit.vue
+++ src/views/LiveModule/edit.vue
@@ -154,7 +154,7 @@
         </div>
         <div class="modeHide" v-if="$route.query.type==2"></div>
       </el-form-item>
-      <el-form-item label="云导播" required v-if="liveMode==2" class="max-column">
+      <el-form-item label="云导播" required v-if="showDelayTag && liveMode==2" class="max-column">
         <div class="titleBox">
           <div class="pageTitle">
             <span v-if="!webinarDirector">云导播活动为付费功能请<a class="blue" target="_blank"  href="https://vhall.s4.udesk.cn/im_client/?web_plugin_id=15038"> 联系客服 </a>开通，点我了解<a class="set-font" href="https://saas-doc.vhall.com/docs/show/1451" target="_blank">云导播活动</a></span>
