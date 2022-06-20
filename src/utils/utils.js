3fbcd545fbab55f2e701deb607b3b764c07086f5
qingzhao.xu <qingzhao.xu@vhall.com>
2022-06-20 11:17:13 +0800

    feat: 自动登录

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -26,7 +26,7 @@ export const sessionOrLocal = {
   }
 };
 // 判断是否IE
-export function isIE () {
+export function isIE() {
   return (!!window.ActiveXObject || 'ActiveXObject' in window || navigator.userAgent.indexOf("Edge") > -1);
 }
 
@@ -39,7 +39,7 @@ export function browserDetect() {
 }
 
 // 尺寸重置
-export function resize () {
+export function resize() {
   if (isIE()) {
     let evt = window.document.createEvent('UIEvents');
     evt.initUIEvent('resize', true, false, window, 0);
@@ -51,9 +51,9 @@ export function resize () {
 }
 
 // 防抖
-export const debounce = (function () {
+export const debounce = (function() {
   let timer = 0
-  return function (callback, ms) {
+  return function(callback, ms) {
     clearTimeout(timer)
     timer = setTimeout(callback, ms)
   }
@@ -64,21 +64,21 @@ export const throttle = function(func, delay) {
   let timer = null;
   let startTime = Date.now();
   return function() {
-      const curTime = Date.now();
-      const remaining = delay - (curTime - startTime);
-      const context = this;
-      const args = arguments;
-      clearTimeout(timer);
-      if (remaining <= 0) {
-          func.apply(context, args);
-          startTime = Date.now();
-      } else {
-          timer = setTimeout(func, remaining);
-      }
+    const curTime = Date.now();
+    const remaining = delay - (curTime - startTime);
+    const context = this;
+    const args = arguments;
+    clearTimeout(timer);
+    if (remaining <= 0) {
+      func.apply(context, args);
+      startTime = Date.now();
+    } else {
+      timer = setTimeout(func, remaining);
+    }
   }
 }
 
-export function calculateAudioLevel (level) {
+export function calculateAudioLevel(level) {
   let audioLevelValue = 1;
   if (level > 0 && level <= 0.04) {
     audioLevelValue = 1;
@@ -94,7 +94,7 @@ export function calculateAudioLevel (level) {
   return audioLevelValue;
 }
 
-export function calculateNetworkStatus (status) {
+export function calculateNetworkStatus(status) {
   let netWorkStatus = 2;
   if (status >= 0 && status <= 2) {
     netWorkStatus = 2;
@@ -108,8 +108,8 @@ export function calculateNetworkStatus (status) {
   return netWorkStatus;
 }
 
-export function copy(text){
-  return new Promise((resolve, reject)=>{
+export function copy(text) {
+  return new Promise((resolve, reject) => {
     const inputEl = document.createElement('input');
     inputEl.type = 'text';
     inputEl.value = text;
@@ -119,9 +119,9 @@ export function copy(text){
     inputEl.select();
     try {
       let result = document.execCommand('copy');
-      if(result){
+      if (result) {
         resolve();
-      }else{
+      } else {
         reject(new Error('浏览器暂不支持复制功能，请升级浏览器'));
       }
 
@@ -135,22 +135,22 @@ export function copy(text){
 /**
  *  判断传入参数的类型，以字符串的形式返回
  **/
-export function dataType(obj){
+export function dataType(obj) {
   if (obj === null) return "Null";
   if (obj === undefined) return "Undefined";
-  return Object.prototype.toString.call(obj).slice(8,-1);
+  return Object.prototype.toString.call(obj).slice(8, -1);
 }
 
 /**
  * 处理对象参数值，排除对象参数值为”“、null、undefined，并返回一个新对象
  **/
-export function dealObjectValue(obj){
+export function dealObjectValue(obj) {
   let param = {};
-  if ( obj === null || obj === undefined || obj === "" ) return param;
-  for ( let key in obj ){
-    if (dataType(obj[key]) === "Object" ){
+  if (obj === null || obj === undefined || obj === "") return param;
+  for (let key in obj) {
+    if (dataType(obj[key]) === "Object") {
       param[key] = dealObjectValue(obj[key]);
-    }else if(obj[key] !== null && obj[key] !== undefined && obj[key] !== ""){
+    } else if (obj[key] !== null && obj[key] !== undefined && obj[key] !== "") {
       param[key] = obj[key];
     }
   }
@@ -165,7 +165,7 @@ export function domainCovert(baseDomain, url) {
   // 若value不包含域名，拼接展示。
   if (url === '' || url === undefined || url === null) {
     return url;
-  } else if(url.indexOf(baseDomain) !== -1) {
+  } else if (url.indexOf(baseDomain) !== -1) {
     return url;
   } else {
     return `${baseDomain}${url}`;
@@ -179,11 +179,11 @@ export function domainCovert(baseDomain, url) {
  * //t-vhallsaas-static.oss-cn-beijing.aliyuncs.com/upload/
  **/
 export function parseURL(url) {
- /* let a = document.createElement('a');
-  a.href = url;
-  return {
-    path: a.pathname.replace(/^([^\/])/,'/$1').replace('/upload/', '')
-  };*/
+  /* let a = document.createElement('a');
+   a.href = url;
+   return {
+     path: a.pathname.replace(/^([^\/])/,'/$1').replace('/upload/', '')
+   };*/
   // 查找/upload/位置，保留之后的路径
 
   let targetIndex = url.indexOf('/upload/');
@@ -234,9 +234,9 @@ export function checkUploadType(file, that, type = 1) {
   let imgSrc = window.URL.createObjectURL(file);
   let img = new Image();
   img.src = imgSrc;
-  img.onload = function () {
+  img.onload = function() {
     // 我在这里就可以获取到图片的宽度和高度了 img.width 、img.height
-    if (img.width !== 1280 && img.height !== 720 ) {
+    if (img.width !== 1280 && img.height !== 720) {
       that.$message({
         message: `请上传1280*720尺寸图片`,
         showClose: true,
@@ -251,29 +251,28 @@ export function checkUploadType(file, that, type = 1) {
 }
 
 export function getQueryString(name) {
-  let reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+  let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
   let r = window.location.search.substr(1).match(reg);
-  if(r!=null)return  unescape(r[2]); return null;
+  if (r != null) return unescape(r[2]); return null;
 }
 // 判断是否登录成功
 export function checkAuth(to, from, next, that) {
   let isOld = false
-  if(location.search && getQueryString('form') == 1){
+  if (location.search && getQueryString('form') == 1) {
     isOld = true
   }
-  if(to.path.indexOf('/keylogin-host') !== -1 ||
+  if (to.path.indexOf('/keylogin-host') !== -1 ||
     to.path.indexOf('/keylogin') !== -1 || to.path.indexOf('/embedclient') !== -1 ||
     from.path.indexOf('/keylogin') !== -1 ||
     to.path.indexOf('/subscribe') !== -1 ||
     to.path.indexOf('/entryform') !== -1 ||
     to.path.indexOf('/live/watch') !== -1 ||
-    to.path.indexOf('/login') !== -1 ||
     to.path.indexOf('/register') !== -1 ||
     to.path.indexOf('/live/room') !== -1 ||
     to.path.indexOf('/forgetPassword') !== -1 || (to.path.indexOf('/live/room') !== -1 && sessionOrLocal.get('interact_token'))
     || (to.path.indexOf('/chooseWay') !== -1 && sessionOrLocal.get('interact_token')) || to.path.indexOf('/upgrading') !== -1 || to.path.indexOf('/warning/') !== -1
     || to.path.indexOf('/special/detail') != -1 || to.path.indexOf('/cMiddle') != -1
-    ) {
+  ) {
     // 不验证直接进入
     next();
     NProgress.done();
@@ -297,7 +296,7 @@ export function checkAuth(to, from, next, that) {
     fetchData('callbackUserInfo', params).then(res => {
       if (res.data && res.code === 200) {
         // 登录场景下，存储直接登录
-        if(Number(scene_id) === 1) {
+        if (Number(scene_id) === 1) {
           sessionOrLocal.set('token', res.data.token || '', 'localStorage');
           sessionOrLocal.set('tokenExpiredTime', res.data.exp_time, 'localStorage')
           sessionOrLocal.set('sso_token', res.data.sso_token || '');
@@ -305,7 +304,7 @@ export function checkAuth(to, from, next, that) {
         }
         // 非观看页第三方登录场景，均跳转/home
         if (!sourceTag) {
-          if(auth_tag) {
+          if (auth_tag) {
             if (auth_tag.indexOf('bind') !== -1) {
               // 绑定成功
               window.location.href = `${window.location.origin}${process.env.VUE_APP_WEB_KEY}/acc/info?tab=2`;
@@ -317,7 +316,7 @@ export function checkAuth(to, from, next, that) {
         }
       } else {
         // 非200情况下，若是3账户信息-账号绑定，提示当前账号已绑定，请解绑。
-        if(auth_tag) {
+        if (auth_tag) {
           if (auth_tag.indexOf('bind') !== -1) {
             sessionOrLocal.set('bind_result', JSON.stringify(res));
             sessionOrLocal.set('user_auth_key', user_auth_key);
@@ -335,7 +334,7 @@ export function checkAuth(to, from, next, that) {
             sessionOrLocal.clear('localStorage');
             sessionOrLocal.clear();
           }
-        } else{
+        } else {
           that.$message({
             message: res.msg || '异常请求，无法操作',
             showClose: true,
@@ -349,7 +348,7 @@ export function checkAuth(to, from, next, that) {
       }
     }).catch(e => {
       // 非200情况下，若是3账户信息-账号绑定，提示当前账号已绑定，请解绑。
-      if(auth_tag) {
+      if (auth_tag) {
         if (auth_tag.indexOf('bind') !== -1) {
           sessionOrLocal.set('bind_result', JSON.stringify(e));
           sessionOrLocal.set('user_auth_key', user_auth_key);
@@ -367,7 +366,7 @@ export function checkAuth(to, from, next, that) {
           sessionOrLocal.clear('localStorage');
           sessionOrLocal.clear();
         }
-      } else{
+      } else {
         // 获取回调token失败
         that.$message({
           message: e.msg || '登录信息获取失败，请重新登录',
@@ -393,9 +392,9 @@ export function checkAuth(to, from, next, that) {
       return;
     }
     fetchData('planFunctionGet', {}).then(res => {
-      if(res && res.code === 200) {
+      if (res && res.code === 200) {
         let permissions = res.data.permissions;
-        if(permissions) {
+        if (permissions) {
           // 设置全部权限
           sessionOrLocal.set('SAAS_VS_PES', permissions, 'localStorage');
         } else {
@@ -407,27 +406,27 @@ export function checkAuth(to, from, next, that) {
       let out_url = sessionOrLocal.get('SAAS_V3_CTRL_OUT', 'localStorage');
       sessionStorage.clear()
       localStorage.clear()
-      if(e.code == 511006){
-        if(out_url) {
+      if (e.code == 511006) {
+        if (out_url) {
           // 清除cookies
           Cookies.remove('gray-id');
           window.location.href = out_url
         } else {
-          next({path: '/login', query:{'form': isOld == 1 ? 1 : 0}});
+          next({ path: '/login', query: { 'form': isOld == 1 ? 1 : 0 } });
         }
       }
       sessionOrLocal.removeItem('SAAS_VS_PES');
     });
     // 登录后，获取用户基本信息
-    fetchData('getInfo', {scene_id: 2}).then(res => {
+    fetchData('getInfo', { scene_id: 2 }).then(res => {
       // debugger;
-      if(res.code === 200) {
-        if(res.data.is_new_regist == 2) {
+      if (res.code === 200) {
+        if (res.data.is_new_regist == 2) {
           sessionOrLocal.clear();
           sessionOrLocal.clear('localStorage');
           // 清除cookies
           Cookies.remove('gray-id');
-          next({path: '/upgrading'});
+          next({ path: '/upgrading' });
           NProgress.done();
         }
         sessionOrLocal.set('userInfo', JSON.stringify(res.data));
@@ -440,7 +439,7 @@ export function checkAuth(to, from, next, that) {
             webinar_user_id: res.data.user_id,
             scene_id: 1,
           }).then(res => {
-            if(res && res.code === 200 && res.data.permissions) {
+            if (res && res.code === 200 && res.data.permissions) {
               // 设置活动全部权限
               sessionOrLocal.set('WEBINAR_PES', permissions, 'localStorage');
             }
@@ -450,37 +449,41 @@ export function checkAuth(to, from, next, that) {
         }
       } else {
         sessionOrLocal.set('userInfo', null);
+        sessionOrLocal.removeItem('token'); // 无效token, 删除token
       }
       // 获取子账号数据
       fetchData('sonCountGet', {}).then(result => {
-        if( result && result.code === 200) {
+        if (result && result.code === 200) {
           sessionOrLocal.set(SAAS_V3_COL.KEY_1, JSON.stringify(result.data || {}));
         }
-      }).catch(e=> {});
+      }).catch(e => { });
       next();
       NProgress.done();
-    }).catch(e=>{
+    }).catch(e => {
       console.log(e);
       let out_url = sessionOrLocal.get('SAAS_V3_CTRL_OUT', 'localStorage');
-      if(out_url) {
+      if (out_url) {
         // 清除cookies
         Cookies.remove('gray-id');
         sessionOrLocal.clear();
         sessionOrLocal.clear('localStorage');
         window.location.href = out_url
       } else {
-        next({path: '/login'});
+        next({ path: '/login' });
         NProgress.done();
       }
     });
   } else {
     // 若无token，专题详情、个人主页亦是可以登录得
-    if(to.path.indexOf('/special/detail') !== -1 || to.path.indexOf('/user/home') !== -1) {
+    // 清空缓存后的登录页必须通行(否则死循环)
+    const pathArr = ['/special/detail', '/user/home', '/login']
+    if (pathArr.includes(to.path)) {
       next();
       NProgress.done();
       return;
     }
-    next({path: '/login', query:{'form': isOld == 1 ? 1 : 0}});
+    // 其余退回登录页
+    next({ path: '/login', query: { 'form': isOld == 1 ? 1 : 0 } });
     NProgress.done();
   }
 }
@@ -557,7 +560,7 @@ export const clearCookies = () => {
   const keys = document.cookie.match(/[^ =;]+(?=\\=)/g);
   if (keys) {
     for (let i = keys.length; i--;)
-    document.cookie = keys[i] + '=0;expires=' + new Date(0).toUTCString()
+      document.cookie = keys[i] + '=0;expires=' + new Date(0).toUTCString()
   }
   // TODO 重点关注如何清空缓存key
   Cookies.remove('token', { path: '' })
