c7bcc9d23b7469086f7dff071a00954f681d015b
qingzhao.xu <qingzhao.xu@vhall.com>
2022-06-23 14:58:38 +0800

    feat: 自动登录增加对cookie的验证

--- src/utils/ssoAutoLogin.js
+++ src/utils/ssoAutoLogin.js
@@ -1,12 +1,29 @@
 import fetchData from "@/api/fetch";
 import { sessionOrLocal } from '@/utils/utils'
+import Cookies from 'js-cookie';
+const bizId = 2 // 化蝶的业务线id
 let loaded = false // 是否已加载
 
+/**
+ * @description 验证cookie是否需要请求接口置换token
+ * @returns true 需要, false 不需要
+ */
+function verifyCookie() {
+  const ssoToken = Cookies.get('sso_token') // 用户中心的用户token
+  const ssoLoginStatus = Cookies.get('sso_login_status') // 用户在用户中心的状态(1为登录)
+  const ssoTokenOrigin = Cookies.get('sso_token_origin') // 用户登录的业务线
+  const ssoTokenAfterLogin = Cookies.get('sso_token_after_login') // 用户登录后给予的token(验证防止多次请求)
+  if (!ssoToken) return false
+  if (ssoLoginStatus != 1) return false
+  if (ssoTokenOrigin == bizId) return false
+  return ssoToken !== ssoTokenAfterLogin
+}
+
 const ssoAutoLogin = async () => {
-  if (loaded) return Promise.resolve() // 已经请求过了, 则不再执行后续逻辑
+  if (loaded || !verifyCookie()) return Promise.resolve() // 已经请求过了 或 cookie验证不通过, 则不再执行请求
   return new Promise((resolve) => {
     fetchData('ssoAutoLogin', {
-      biz_id: 2
+      biz_id: bizId
     }, {
       credentials: true // 携带cookie 配置
     }).then(res => {
