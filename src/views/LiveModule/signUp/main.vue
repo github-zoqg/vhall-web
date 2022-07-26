54a9ebcc448208916179192b97761a868e696123
jia.li <jia.li@vhall.com>
2022-07-26 14:50:54 +0800

    feat: 提交报名表单提交

--- src/views/LiveModule/signUp/main.vue
+++ src/views/LiveModule/signUp/main.vue
@@ -106,16 +106,9 @@ export default {
       }
     }
   },
-  created(){
+  async created(){
     this.userId = JSON.parse(sessionOrLocal.get('userId'));
-    if (this.signUpPageType === 'subject') {
-      // 专题下是否开启，只要引用了报名表单，默认就是开启的
-      this.signUpSwtich = true
-      // 更新表单组件里的字段展示
-      this.$nextTick(() => {
-        this.$refs.signSetFormDom && this.$refs.signSetFormDom.setSwitchStatus(this.signUpSwtich)
-      })
-    }
+    await this.getBaseInfo();
   },
   async mounted() {
     this.$refs.signSetFormDom && this.$refs.signSetFormDom.initComp()
@@ -160,8 +153,10 @@ export default {
       this.$fetch('regFromGet', this.setParamsIdByRoute({})).then(res => {
         if (res.code === 200) {
           this.baseInfo = res.data;
-          if (this.signUpPageType === 'webinar') {
-            // 活动下是否开启，通过接口拿取
+          if (this.signUpPageType === 'subject') {
+            // 专题下是否开启，只要引用了报名表单，默认就是开启的
+            this.signUpSwtich = true
+          } else {
             this.signUpSwtich = res.data.enable_status == '0' ? false : true;
           }
         }
