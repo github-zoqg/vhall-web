27a94161edd209e85dedd3952ed0bf7938f0364b
xiao.zhang <xiao.zhang@vhall.com>
2022-08-03 17:03:25 +0800

    feat: 修改

---
+++ src/common/skins/theme-default-black.js
@@ -0,0 +1,21 @@
+/**
+ * 黑色主题颜色值
+ */
+
+/* 字体颜色 */
+
+const color_text_primary = '#FFF';
+const color_text_regular = '#E6E6E6';
+const color_text_secondary = '#A6A6A6';
+const color_text_third = 'rgba(255, 255, 255, 0.26)';
+const color_text_placeholder = 'rgba(255, 255, 255, 0.35)';
+const color_text_active = '#FB2626';
+
+
+/* 背景颜色 */
+const background_color_default = '#000';
+const background_color_primary = '#1a1a1a';
+const background_color_regular = '#2a2a2a';
+const background_color_secondary = 'rgba(255, 255, 255, 0.08)';
+const background_color_third = '#C0C4CC';
+const background_color_active = '#FB2626';
