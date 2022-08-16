8aa403d2e9ebf6e669cb7e2754b53b32acc75b31
xiao.zhang <xiao.zhang@vhall.com>
2022-08-16 10:17:54 +0800

    feat: 添加变量

---
+++ src/common/skins/wap/style_3_theme_4.js
@@ -0,0 +1,39 @@
+/**
+ * 黑色主题颜色值
+ */
+
+/* 字体颜色 */
+
+const color_text_primary = '#FFF';
+const color_text_regular = '#E6E6E6';
+const color_text_secondary = '#A6A6A6';
+const color_text_footer = 'rgba(255, 255, 255, 0.26)';
+const color_text_send = 'rgba(255, 255, 255, 0.35)';
+const color_text_chatName = '#737373';
+const color_text_active = '#FB2626';
+
+
+/* 背景颜色 */
+const background_color_theme = '#1a1a1a';
+const background_color_default = '#000';
+const background_color_primary = '#1a1a1a';
+const background_color_regular = '#2a2a2a';
+const background_color_chatBg = 'rgba(255, 255, 255, 0.08)';  //聊天文案背景
+const background_color_active = '#FB2626';
+
+
+export default {
+  '--color_text_primary': color_text_primary,
+  '--color_text_regular': color_text_regular,
+  '--color_text_secondary': color_text_secondary,
+  '--color_text_footer': color_text_footer,
+  '--color_text_send': color_text_send,
+  '--color_text_chatName': color_text_chatName,
+  '--color_text_active': color_text_active,
+  '--background_color_default': background_color_default,
+  '--background_color_primary': background_color_primary,
+  '--background_color_regular': background_color_regular,
+  '--background_color_chatBg': background_color_chatBg,
+  '--background_color_active': background_color_active,
+  '--background_color_theme': background_color_theme
+}
