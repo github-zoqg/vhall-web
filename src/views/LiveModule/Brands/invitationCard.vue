6c8bde74e7b0470d4f0b4cacde20602d1822fde8
qiang.zhao <qiang.zhao@vhall.com>
2022-08-18 18:41:36 +0800

    代码还原

--- src/views/LiveModule/Brands/invitationCard.vue
+++ src/views/LiveModule/Brands/invitationCard.vue
@@ -850,8 +850,7 @@ export default {
       border-radius: 4px;
       border: 1px solid #e6e6e6;
       background-image: url('../../../common/images/v35-webinar.png');
-      background-size: cover;
-      background-position: left top;
+      background-size: 100% 100%;
       height: 622px;
       .show-container {
         margin: 50px 24px;
@@ -969,8 +968,7 @@ export default {
       // box-shadow: 0px 10px 40px 0px rgba(0, 0, 0, 0.5);
       // height: 360px;
       background-image: url('../../../common/images/v35-webinar.png');
-      background-size: cover;
-      background-position: left top;
+      background-size: 100% 100%;
       background-repeat: no-repeat;
       border-radius: 4px;
       z-index: 0;
@@ -1099,8 +1097,7 @@ export default {
       border-radius: 4px;
       // border: 1px solid #E2E2E2;
       background-image: url('../../../common/images/v35-webinar.png');
-      background-size: cover;
-      background-position: left top;
+      background-size: 100% 100%;
       background-repeat: no-repeat;
       position: relative;
       .look-color-shadow {
