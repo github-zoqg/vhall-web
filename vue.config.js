2a3cab4268969a4a1a349bd217bc303350cf20e3
qiang.zhao <qiang.zhao@vhall.com>
2022-10-10 14:13:30 +0800

    问卷生产版本号更新

--- vue.config.js
+++ vue.config.js
@@ -11,9 +11,7 @@ let cdn = {
     '//static.vhallyun.com/jssdk/vhall-jssdk-doc/latest/vhall-jssdk-doc-3.1.6.js',
     '//s1.e.vhall.com/common-static/middle/vue/2.6.14/dist/vue.min.js',
     // '//cnstatic01.e.vhall.com/3rdlibs/common-libs/ui-frame/element-UI.js',
-    // 'http://localhost:2567/questionnaire_service.js',
-    'https://t-vhallsaas-static.oss-cn-beijing.aliyuncs.com/common-static/middle/questionnaire/1.0.7-test3/questionnaire_service.js',
-    // '//s3.e.vhall.com/common-static/middle/questionnaire-web/1.0.6/questionnaire_service.js',
+    '//s3.e.vhall.com/common-static/middle/questionnaire-web/1.0.7/questionnaire_service.js',
     '//cnstatic01.e.vhall.com/common-static/middle/vhall-ui/v1.1.0/index.js'
   ],
   css: [
