8ba9e35d3d065e4bca2e3757d9d2f501f1c96232
qiang.zhao <qiang.zhao@vhall.com>
2022-10-14 18:04:54 +0800

    问卷测试包

--- vue.config.js
+++ vue.config.js
@@ -11,7 +11,8 @@ let cdn = {
     '//static.vhallyun.com/jssdk/vhall-jssdk-doc/latest/vhall-jssdk-doc-3.1.6.js',
     '//s1.e.vhall.com/common-static/middle/vue/2.6.14/dist/vue.min.js',
     // '//cnstatic01.e.vhall.com/3rdlibs/common-libs/ui-frame/element-UI.js',
-    '//s3.e.vhall.com/common-static/middle/questionnaire-web/1.0.7/questionnaire_service.js',
+    // '//s3.e.vhall.com/common-static/middle/questionnaire-web/1.0.7/questionnaire_service.js',
+    'https://t-vhallsaas-static.oss-cn-beijing.aliyuncs.com/common-static/middle/questionnaire/1.0.7-test1/questionnaire_service.js',
     '//cnstatic01.e.vhall.com/common-static/middle/vhall-ui/v1.1.0/index.js'
   ],
   css: [
