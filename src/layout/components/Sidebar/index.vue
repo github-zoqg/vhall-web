9f8154baac99054b0948c2d01091ac36f7da7859
qiang.zhao <qiang.zhao@vhall.com>
2022-12-09 15:30:43 +0800

    七牛云类型判断

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,23 +1,35 @@
 <template>
-  <div :class="{'has-logo':showLogo}">
-    <div class="sidebar-logo-container" :class="{'collapse':!sidebar.opened}">
+  <div :class="{ 'has-logo': showLogo }">
+    <div
+      class="sidebar-logo-container"
+      :class="{ collapse: !sidebar.opened, isQn: userInfo.user_extends.extends_remark == 2 }"
+    >
       <transition>
         <!-- 关闭情况下 -->
         <router-link v-if="!sidebar.opened" key="collapse" class="sidebar-logo-link" to="/">
-          <img v-if="logo" :src="logo" class="sidebar-logo">
+          <img v-if="logo" :src="logo" class="sidebar-logo" />
         </router-link>
         <!-- 展开情况下 -->
-        <div v-else key="expand" class="sidebar-logo-link">
+        <div
+          v-else
+          key="expand"
+          class="sidebar-logo-link"
+          :class="{ isQn: userInfo.user_extends.extends_remark == 2 }"
+        >
           <a :href="logo_jump_url" v-if="logo" class="sidebar-logo2">
-            <img v-if="logo" :src="logo">
+            <img v-if="logo" :src="logo" />
           </a>
           <a :href="logo_jump_url" v-else class="sidebar-logo2">
-            <img src="../../../common/images/sys/logo@2x.png"  class="sidebar-logo static"/>
+            <img src="../../../common/images/sys/logo@2x.png" class="sidebar-logo static" />
           </a>
         </div>
       </transition>
       <!-- 是否收缩按钮 -->
-      <hamburger :is-active="sidebar.opened" :class="`hamburger-container ${sidebar.opened ? 'right' : 'left' }`" @toggleClick="toggleSideBar" />
+      <hamburger
+        :is-active="sidebar.opened"
+        :class="`hamburger-container ${sidebar.opened ? 'right' : 'left'}`"
+        @toggleClick="toggleSideBar"
+      />
     </div>
     <el-scrollbar wrap-class="scrollbar-wrapper">
       <el-menu
@@ -33,297 +45,335 @@
         mode="vertical"
         class="el-menu-reset"
       >
-      <template  v-for="route in routes">
-         <sidebar-item :key="route.path" :item="route" :base-path="route.path" v-show="isShow(route)">
-         </sidebar-item>
-      </template>
+        <template v-for="route in routes">
+          <sidebar-item
+            :key="route.path"
+            :item="route"
+            :base-path="route.path"
+            v-show="isShow(route)"
+          ></sidebar-item>
+        </template>
       </el-menu>
     </el-scrollbar>
   </div>
 </template>
 
 <script>
-import SidebarItem from './SidebarItem';
-import Hamburger from '../Hamburger/index.vue';
-import Env from '@/api/env.js';
-import {sessionOrLocal} from "@/utils/utils";
-export default {
-  components: {
-    SidebarItem,
-    Hamburger
-  },
-  data() {
-    return {
-      sidebar: {
-        opened: true,
-        withoutAnimation: false
+  import SidebarItem from './SidebarItem';
+  import Hamburger from '../Hamburger/index.vue';
+  import Env from '@/api/env.js';
+  import { sessionOrLocal } from '@/utils/utils';
+  import QnLogo from '@/common/images/temp/QnLogo.png';
+  export default {
+    components: {
+      SidebarItem,
+      Hamburger
+    },
+    data() {
+      return {
+        sidebar: {
+          opened: true,
+          withoutAnimation: false
+        },
+        logo: null,
+        logo_jump_url: process.env.VUE_APP_COMPANY_URL,
+        childPremission: {},
+        userInfo: {}
+      };
+    },
+    computed: {
+      routes() {
+        return this.$router.options.routes;
       },
-      logo: null,
-      logo_jump_url: process.env.VUE_APP_COMPANY_URL,
-      childPremission: {},
-    };
-  },
-  computed: {
-    routes() {
-      return this.$router.options.routes;
-    },
-    activeMenu() {
-      const route = this.$route;
-      const { meta, path } = route;
-      if (meta.activeMenu) {
-        return meta.activeMenu;
+      activeMenu() {
+        const route = this.$route;
+        const { meta, path } = route;
+        if (meta.activeMenu) {
+          return meta.activeMenu;
+        }
+        return path;
+      },
+      showLogo() {
+        return sessionOrLocal.get('sidebarLogo') || false;
       }
-      return path;
     },
-    showLogo() {
-      return sessionOrLocal.get('sidebarLogo') || false;
-    }
-  },
-  methods: {
-    isShow(route) {
-      if (this.childPremission && Number(this.childPremission.permission_data) === 0) {
-        // 子账号有设置权限，数据不展示，碰到name=dataMgr的数据，不展示整个模块。其它模块正常处理
-        if (route && route.meta && route.meta.name === 'dataMgr') {
-          return false
+    methods: {
+      isShow(route) {
+        if (this.childPremission && Number(this.childPremission.permission_data) === 0) {
+          // 子账号有设置权限，数据不展示，碰到name=dataMgr的数据，不展示整个模块。其它模块正常处理
+          if (route && route.meta && route.meta.name === 'dataMgr') {
+            return false;
+          } else {
+            return (
+              route &&
+              route.children &&
+              route.children.length > 0 &&
+              route.children.filter(item => {
+                if (this.vsQuanxian[item.meta.auth_key] > 0) {
+                  return true;
+                } else {
+                  return false;
+                }
+              })
+            );
+          }
         } else {
-          return route && route.children && route.children.length > 0 && route.children.filter(item => {
-            if (this.vsQuanxian[item.meta.auth_key] > 0) {
-              return true
+          // TODO 左侧导航菜单
+          if (route.meta && route.meta.auth_key && this.vsQuanxian) {
+            console.log('左侧', route.meta.auth_key, this.vsQuanxian[route.meta.auth_key]);
+            if (this.vsQuanxian[route.meta.auth_key] > 0) {
+              // 由权限限制的字段，若大于0，则返回
+              return true;
             } else {
-              return false
+              return false;
             }
+          } else {
+            // 七牛账号不展示以下菜单
+            if (
+              this.userInfo.user_extends.extends_remark == 2 &&
+              ['/acc', '/'].includes(route.path)
+            ) {
+              return false;
+            }
+            return true;
+          }
+        }
+      },
+      getChildPermission() {
+        return this.$fetch('getChildPermission')
+          .then(res => {
+            console.log('当前权限', res.data);
+            sessionOrLocal.set('SAAS_V3_SON_PS', JSON.stringify(res.data));
+            this.childPremission = res.data;
           })
+          .catch(res => {
+            sessionOrLocal.removeItem('SAAS_V3_SON_PS');
+            this.childPremission = {};
+          });
+      },
+      // 开启或者关闭 左侧导航部分
+      toggleSideBar() {
+        this.sidebar.opened = !this.sidebar.opened;
+        sessionOrLocal.set('v3-control-sidebar', JSON.stringify(this.sidebar));
+        this.$EventBus.$emit('hamburger', this.sidebar.opened);
+      },
+      changeImg() {
+        console.log(111111111111);
+      },
+      selectMenu(index, indexPath) {
+        switch (index) {
+          case '/material/word':
+            this.setReport(100510);
+            break;
+          case '/material/video':
+            this.setReport(100517);
+            break;
+          case '/material/question':
+            this.setReport(100525);
+            break;
+          case '/material/prize':
+            this.setReport(100533);
+            break;
+          case '/material/viewer':
+            this.setReport(100540);
+            break;
+          case '/material/advertCard':
+            this.setReport(100550);
+            break;
+          case '/material/gift':
+            this.setReport(100556);
+            break;
+          case '/data/info':
+            this.setReport(100562);
+            break;
+          case '/data/live':
+            this.setReport(100577);
+            break;
+          case '/setting/function':
+            this.setReport(100620);
+            break;
+          case '/setting/dev':
+            this.setReport(100589);
+            break;
+          case '/setting/logo':
+            this.setReport(100615);
+            break;
+          case '/setting/chat':
+            this.setReport(100580);
+            break;
+          case '/setting/brand':
+            this.setReport(100633);
+            break;
+          case '/setting/player':
+            this.setReport(100490);
+            break;
+          case '/finance/info':
+            this.setReport(100688);
+            break;
+          case '/finance/income':
+            this.setReport(100749);
+            break;
+          case '/acc/info':
+            this.setReport(100777);
+            break;
+          case '/acc/myHome':
+            this.setReport(100790);
+            break;
+          case '/acc/son':
+            this.setReport(100805);
+            break;
         }
-      } else {
-        // TODO 左侧导航菜单
-        if (route.meta && route.meta.auth_key && this.vsQuanxian) {
-           console.log('左侧', route.meta.auth_key, this.vsQuanxian[route.meta.auth_key])
-           if(this.vsQuanxian[route.meta.auth_key] > 0) {
-             // 由权限限制的字段，若大于0，则返回
-             return true;
-           } else {
-             return false;
-           }
-        } else {
-          return true;
+      },
+      setReport(k) {
+        let userId = sessionOrLocal.get('userId');
+        this.$vhall_paas_port({
+          k: k,
+          data: {
+            business_uid: userId,
+            user_id: '',
+            webinar_id: '',
+            refer: '',
+            s: '',
+            report_extra: {},
+            ref_url: '',
+            req_url: ''
+          }
+        });
+      },
+      pageResize() {
+        this.$nextTick(() => {
+          let screenWidth = window.screen.width;
+          if (screenWidth < 1366) {
+            this.sidebar.opened = false;
+            sessionOrLocal.set('v3-control-sidebar', JSON.stringify(this.sidebar));
+            this.$EventBus.$emit('hamburger', false);
+          }
+        });
+      }
+    },
+    async created() {
+      let userInfo = sessionOrLocal.get('userInfo');
+      this.userInfo = JSON.parse(userInfo);
+      let vsPersonStr = sessionOrLocal.get('SAAS_VS_PES', 'localStorage');
+      if (vsPersonStr) {
+        this.vsQuanxian = JSON.parse(vsPersonStr);
+      }
+      if (userInfo) {
+        if (JSON.parse(userInfo).parent_id > 0) {
+          // 获取子账号权限，更新
+          await this.getChildPermission();
         }
       }
     },
-    getChildPermission() {
-      return this.$fetch('getChildPermission').then(res => {
-        console.log('当前权限', res.data)
-        sessionOrLocal.set('SAAS_V3_SON_PS', JSON.stringify(res.data));
-        this.childPremission = res.data;
-      }).catch(res => {
-        sessionOrLocal.removeItem('SAAS_V3_SON_PS');
-        this.childPremission = {};
-      })
-    },
-    // 开启或者关闭 左侧导航部分
-    toggleSideBar() {
-      this.sidebar.opened = !this.sidebar.opened;
-      sessionOrLocal.set('v3-control-sidebar', JSON.stringify(this.sidebar));
-      this.$EventBus.$emit('hamburger', this.sidebar.opened);
-    },
-    changeImg() {
-      console.log(111111111111);
-    },
-    selectMenu(index, indexPath) {
-      switch (index) {
-        case '/material/word':
-          this.setReport(100510)
-          break;
-        case '/material/video':
-          this.setReport(100517)
-          break;
-        case '/material/question':
-          this.setReport(100525)
-          break;
-        case '/material/prize':
-          this.setReport(100533)
-          break;
-        case '/material/viewer':
-          this.setReport(100540)
-          break;
-        case '/material/advertCard':
-          this.setReport(100550)
-          break;
-        case '/material/gift':
-          this.setReport(100556)
-          break;
-        case '/data/info':
-          this.setReport(100562)
-          break;
-        case '/data/live':
-          this.setReport(100577)
-          break;
-        case '/setting/function':
-          this.setReport(100620)
-          break;
-        case '/setting/dev':
-          this.setReport(100589)
-          break;
-        case '/setting/logo':
-          this.setReport(100615)
-          break;
-        case '/setting/chat':
-          this.setReport(100580)
-          break;
-        case '/setting/brand':
-          this.setReport(100633)
-          break;
-        case '/setting/player':
-          this.setReport(100490)
-          break;
-        case '/finance/info':
-          this.setReport(100688)
-          break;
-        case '/finance/income':
-          this.setReport(100749)
-          break;
-        case '/acc/info':
-          this.setReport(100777)
-          break;
-        case '/acc/myHome':
-          this.setReport(100790)
-          break;
-        case '/acc/son':
-          this.setReport(100805)
-          break;
+    mounted() {
+      // 从缓存中获取控制台图片
+      let userInfo = JSON.parse(sessionOrLocal.get('userInfo'));
+      this.logo = userInfo.user_extends ? userInfo.user_extends.logo : '';
+      if (userInfo.user_extends.extends_remark == 2) {
+        this.logo = QnLogo;
       }
-    },
-    setReport(k) {
-      let userId = sessionOrLocal.get('userId');
-      this.$vhall_paas_port({
-        k: k,
-        data: {business_uid: userId, user_id: '', webinar_id: '', refer: '', s: '', report_extra: {}, ref_url: '', req_url: ''}
-      })
-    },
-    pageResize(){
-      this.$nextTick(()=>{
-        let screenWidth = window.screen.width;
-        if (screenWidth < 1366) {
-         this.sidebar.opened = false;
-         sessionOrLocal.set('v3-control-sidebar', JSON.stringify(this.sidebar));
-         this.$EventBus.$emit('hamburger', false);
-        }
-      })
-    }
-  },
-  async created() {
-    let userInfo = sessionOrLocal.get('userInfo');
-    let vsPersonStr = sessionOrLocal.get('SAAS_VS_PES', 'localStorage');
-    if (vsPersonStr) {
-      this.vsQuanxian = JSON.parse(vsPersonStr);
-    }
-    if (userInfo) {
-      if(JSON.parse(userInfo).parent_id > 0) {
-        // 获取子账号权限，更新
-        await this.getChildPermission();
-      }
-    }
-  },
-  mounted() {
-    // 从缓存中获取控制台图片
-    let userInfo = JSON.parse(sessionOrLocal.get('userInfo'));
-    this.logo = userInfo.user_extends ? userInfo.user_extends.logo : '';
-    console.log(this.logo, process.env.VUE_APP_COMPANY_URL, '2222222222222222')
+      console.log(this.logo, process.env.VUE_APP_COMPANY_URL, '2222222222222222');
 
-    if(this.logo) {
-      this.logo_jump_url = userInfo.user_extends ? userInfo.user_extends.logo_jump_url : process.env.VUE_APP_COMPANY_URL;
-    } else {
-      this.logo_jump_url = process.env.VUE_APP_COMPANY_URL;
-    }
-    // this.$domainCovert(Env.staticLinkVo.uploadBaseUrl, userInfo.user_extends.logo || '') : '';
-    this.$EventBus.$on("hamburger", (status) => {
-      this.sidebar.opened = status;
-    });
-    this.$EventBus.$on("saas_vs_account_change", (res) => {
-      let user_extends = res.user_extends;
-      this.logo = user_extends.logo;
-      console.log(this.logo,process.env.VUE_APP_COMPANY_URL, '1111111111111111')
-      if(this.logo) {
-        this.logo_jump_url = user_extends.logo_jump_url || process.env.VUE_APP_COMPANY_URL;
+      if (this.logo) {
+        this.logo_jump_url = userInfo.user_extends
+          ? userInfo.user_extends.logo_jump_url
+          : process.env.VUE_APP_COMPANY_URL;
       } else {
         this.logo_jump_url = process.env.VUE_APP_COMPANY_URL;
       }
-    });
-    let _this = this;//赋值vue的this
-    window.onresize = () => {
-  　　// 调用methods中的事件
-      _this.pageResize();
+      // this.$domainCovert(Env.staticLinkVo.uploadBaseUrl, userInfo.user_extends.logo || '') : '';
+      this.$EventBus.$on('hamburger', status => {
+        this.sidebar.opened = status;
+      });
+      this.$EventBus.$on('saas_vs_account_change', res => {
+        let user_extends = res.user_extends;
+        this.logo = user_extends.logo;
+        console.log(this.logo, process.env.VUE_APP_COMPANY_URL, '1111111111111111');
+        if (this.logo) {
+          this.logo_jump_url = user_extends.logo_jump_url || process.env.VUE_APP_COMPANY_URL;
+        } else {
+          this.logo_jump_url = process.env.VUE_APP_COMPANY_URL;
+        }
+      });
+      let _this = this; //赋值vue的this
+      window.onresize = () => {
+        // 调用methods中的事件
+        _this.pageResize();
+      };
+    },
+    destroyed() {
+      window.onresize = null;
     }
-  },
-  destroyed() {
-    window.onresize = null;
-  }
-};
+  };
 </script>
 <style lang="less" scoped>
-/*LOGO部分*/
-.sidebarLogoFade-enter-active {
-  transition: opacity 1.5s;
-}
-.sidebarLogoFade-enter,
-.sidebarLogoFade-leave-to {
-  opacity: 0;
-}
-.sidebar-logo-container {
-  position: relative;
-  width: 100%!important;
-  height: 64px;
-  background: #FB3A32;
-  text-align: center;
-  overflow: hidden;
-  & .sidebar-logo-link {
-    width: 100%;
+  /*LOGO部分*/
+  .sidebarLogoFade-enter-active {
+    transition: opacity 1.5s;
+  }
+  .sidebarLogoFade-enter,
+  .sidebarLogoFade-leave-to {
+    opacity: 0;
+  }
+  .sidebar-logo-container {
+    position: relative;
+    width: 100% !important;
     height: 64px;
+    background: #fb3a32;
     text-align: center;
-    & .sidebar-logo {
-      height: 100%;
+    overflow: hidden;
+    & .sidebar-logo-link {
       width: 100%;
-      /* &.static {
+      height: 64px;
+      text-align: center;
+      & .sidebar-logo {
+        height: 100%;
+        width: 100%;
+        /* &.static {
         height: auto;
         width: 156px;
         float: left;
         margin: 4px 0;
       } */
-    }
+      }
 
-    a.sidebar-logo2 {
-      float: left;
-      margin: 10px 0 10px 18px;
-      text-align: left;
-      width: 120px!important;
-      height: 44px;
-      img {
-        display: block;
-        width: 100%;
-        height: 100%;
-        object-fit: scale-down;
+      a.sidebar-logo2 {
+        float: left;
+        margin: 10px 0 10px 18px;
+        text-align: left;
+        width: 120px !important;
+        height: 44px;
+        img {
+          display: block;
+          width: 100%;
+          height: 100%;
+          object-fit: scale-down;
+        }
+      }
+    }
+    &.collapse {
+      .sidebar-logo {
+        margin-right: 0;
       }
     }
   }
-  &.collapse {
-    .sidebar-logo {
-      margin-right: 0;
+  /*收缩按钮部分*/
+  .hamburger-container {
+    position: absolute;
+    top: calc(50% - 26px);
+    line-height: 44px;
+    height: 44px;
+    cursor: pointer;
+    transition: background 0.3s;
+    -webkit-tap-highlight-color: transparent;
+    &.right {
+      right: 0;
+    }
+    &.left {
+      left: 0;
     }
   }
-}
-/*收缩按钮部分*/
-.hamburger-container {
-  position: absolute;
-  top: calc(50% - 26px);
-  line-height: 44px;
-  height: 44px;
-  cursor: pointer;
-  transition: background 0.3s;
-  -webkit-tap-highlight-color:transparent;
-  &.right {
-    right: 0;
+  .isQn {
+    background: #3562fa;
   }
-  &.left {
-    left: 0;
-  }
-}
 </style>
--- src/views/PlatformModule/Error/index.vue
+++ src/views/PlatformModule/Error/index.vue
@@ -1,147 +1,189 @@
 <template>
   <div>
     <!-- 404 -->
-    <div class="container 404" v-if="$route.params.str !== 'sysUnder' && $route.params.str !== 'network'">
+    <div
+      class="container 404"
+      v-if="$route.params.str !== 'sysUnder' && $route.params.str !== 'network'"
+    >
       <div class="left-section">
         <div class="inner-content">
-          <h1  class="heading">{{$route.params.str}}</h1>
+          <h1 class="heading">{{ $route.params.str }}</h1>
           <!-- 具体文案 -->
           <p class="subheading">页面已失联，攻城狮正在赶来的路上…</p>
           <p class="headingNotice">
-            无法访问本页的原因：<br/>
-            服务器过热导致网站可能出现故障！<br/>
-            所访问的页面不存在或者已被管理员删除！<br/>
+            无法访问本页的原因：
+            <br />
+            服务器过热导致网站可能出现故障！
+            <br />
+            所访问的页面不存在或者已被管理员删除！
+            <br />
           </p>
-          <p class="index-btn"><el-button type="primary" @click.prevent.stop="toReturn" round>返回首页</el-button></p>
+          <p class="index-btn">
+            <el-button type="primary" @click.prevent.stop="toReturn" round>
+              {{ userInfo.user_extends.extends_remark == 2 ? '返回' : '返回首页' }}
+            </el-button>
+          </p>
         </div>
       </div>
       <div class="right-section">
-        <img src="../../../common/images/sys/404.svg" class="svgImg" alt=""/>
+        <img src="../../../common/images/sys/404.svg" class="svgImg" alt="" />
       </div>
     </div>
     <!-- 无服务 -->
     <div class="container sysUnder" v-else-if="$route.params.str === 'sysUnder'">
       <div class="left-section sysUnder">
         <div class="inner-content">
-          <h1  class="heading">系统正在升级中…</h1>
+          <h1 class="heading">系统正在升级中…</h1>
           <!-- 具体文案 -->
           <p class="subheading">亲爱的用户，系统正在升级中，给您带来不便，深表歉意。</p>
         </div>
       </div>
       <div class="right-section">
-        <img src="../../../common/images/sys/no-server.png" class="svgImg" alt=""/>
+        <img src="../../../common/images/sys/no-server.png" class="svgImg" alt="" />
       </div>
     </div>
     <!-- 系统升级 -->
     <div class="container network" v-else>
-      <img src="../../../common/images/sys/no-network.svg" alt=""/>
+      <img src="../../../common/images/sys/no-network.svg" alt="" />
       <p class="heading">网络正在开小差……</p>
-      <p class="index-btn"><el-button type="primary" @click.prevent.stop="toReturn" round>返回首页</el-button></p>
+      <p class="index-btn">
+        <el-button type="primary" @click.prevent.stop="toReturn" round>返回首页</el-button>
+      </p>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: "Error.vue",
-  beforeCreate() {
-  },
-  methods: {
-    toReturn() {
-      this.$router.push({
-        path: '/'
-      });
+  export default {
+    name: 'Error.vue',
+    data() {
+      return {
+        userInfo: {}
+      };
+    },
+    created() {
+      this.userInfo = JSON.parse(sessionStorage.getItem('userInfo'));
+    },
+    methods: {
+      toReturn() {
+        // 七牛返回页
+        if (this.userInfo.user_extends.extends_remark == 2) {
+          window.open(
+            'https://portal.qiniu.com/oauth/v2/authorize?response_type=code&client_id=ZmPTBUkzstLiMqHhrOAPsuVc&redirect_uri=https://test01-saas-api.vhall.com/v3/commons/auth/qny-callback&state=qiniu'
+          );
+        } else {
+          this.$router.push({
+            path: '/'
+          });
+        }
+      }
     }
-  }
-};
+  };
 </script>
 
 <style lang="less" scoped>
-.left-section.sysUnder {
-  width: 416px;
-  margin-right: 77px;
-}
-.left-section .inner-content {
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-}
-.background {
-  background: #ffffff;
-}
-.container {
-  position: relative;
-  margin: 0 auto;
-  width: 52%;
-  height: 100vh;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  .index-btn {
-    margin-top: 32px;
-    text-align: left;
+  .left-section.sysUnder {
+    width: 416px;
+    margin-right: 77px;
   }
-  .heading {
-    font-size: 80px;
-    font-family: @fontSemibold;
-    font-weight: 600;
-    color: #1A1A1A;
-    line-height: 112px;
+  .left-section .inner-content {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
   }
-  &.sysUnder {
-    .heading {
-      font-size: 32px;
-      font-family: @fontRegular;
-      font-weight: 400;
-      color: #1A1A1A;
-      line-height: 45px;
-    }
+  .background {
+    background: #ffffff;
   }
-  &.network {
-    flex-direction: column;
-    width: 245px;
-    height: auto;
-    text-align: center;
-    margin-top: calc((100vh - 292px)/2);
-    .heading {
-      font-size: 14px;
-      font-family: @fontRegular;
-      font-weight: 400;
-      color: #666666;
-      line-height: 20px;
-    }
+  .container {
+    position: relative;
+    margin: 0 auto;
+    width: 52%;
+    height: 100vh;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
     .index-btn {
+      margin-top: 32px;
+      text-align: left;
+    }
+    .heading {
+      font-size: 80px;
+      font-family: @fontSemibold;
+      font-weight: 600;
+      color: #1a1a1a;
+      line-height: 112px;
+    }
+    &.sysUnder {
+      .heading {
+        font-size: 32px;
+        font-family: @fontRegular;
+        font-weight: 400;
+        color: #1a1a1a;
+        line-height: 45px;
+      }
+    }
+    &.network {
+      flex-direction: column;
+      width: 245px;
+      height: auto;
       text-align: center;
+      margin-top: calc((100vh - 292px) / 2);
+      .heading {
+        font-size: 14px;
+        font-family: @fontRegular;
+        font-weight: 400;
+        color: #666666;
+        line-height: 20px;
+      }
+      .index-btn {
+        text-align: center;
+      }
     }
   }
-}
-.left-section {
-  width: 40%;
-}
-.subheading {
-  height: 22px;
-  font-size: 16px;
-  font-family: @fontRegular;
-  font-weight: 400;
-  color: #1A1A1A;
-  line-height: 22px;
-}
-.headingNotice {
-  margin-top: 16px;
-  width: 266px;
-  font-size: 14px;
-  font-family: @fontRegular;
-  font-weight: 400;
-  color: #999999;
-  line-height: 22px;
-}
-.right-section {
-  width: 50%;
-}
-.svgImg {
-  position: absolute;
-  top: calc(25%);
-  max-width: 100%;
-  max-height: 100%;
-}
+  .left-section {
+    width: 40%;
+  }
+  .subheading {
+    height: 22px;
+    font-size: 16px;
+    font-family: @fontRegular;
+    font-weight: 400;
+    color: #1a1a1a;
+    line-height: 22px;
+  }
+  .headingNotice {
+    margin-top: 16px;
+    width: 266px;
+    font-size: 14px;
+    font-family: @fontRegular;
+    font-weight: 400;
+    color: #999999;
+    line-height: 22px;
+  }
+  .right-section {
+    width: 50%;
+  }
+  .svgImg {
+    position: absolute;
+    top: calc(25%);
+    max-width: 100%;
+    max-height: 100%;
+  }
+  @media screen and (max-width: 750px) {
+    .container {
+      width: 100%;
+    }
+    .left-section {
+      height: 50%;
+      width: 92%;
+      position: relative;
+      top: 33%;
+    }
+    .right-section {
+      width: 100%;
+      position: absolute;
+      top: 0;
+      height: 50%;
+    }
+  }
 </style>
